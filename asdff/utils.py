565e5d83833c31640c7f28cbf1794e083bd3a9bf
Roger Condori <114810545+R3gm@users.noreply.github.com>
2023-10-08 16:37:14 -0400

    Added make_inpaint_condition

--- asdff/utils.py
+++ asdff/utils.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 from diffusers.utils import BaseOutput
 from PIL import Image, ImageFilter, ImageOps
-
+import torch
 
 @dataclass
 class ADOutput(BaseOutput):
@@ -68,3 +68,15 @@ def composite(
     output.paste(resized, bbox_padded)
     output.alpha_composite(img_masked)
     return output.convert("RGB")
+
+
+
+def make_inpaint_condition(init_image, mask_image):
+    init_image = np.array(init_image.convert("RGB")).astype(np.float32) / 255.0
+    mask_image = np.array(mask_image.convert("L")).astype(np.float32) / 255.0
+
+    assert init_image.shape[0:1] == mask_image.shape[0:1], "image and image_mask must have the same image size"
+    init_image[mask_image > 0.5] = -1.0  # set as masked pixel
+    init_image = np.expand_dims(init_image, 0).transpose(0, 3, 1, 2)
+    init_image = torch.from_numpy(init_image)
+    return init_image
